26e90737a57ca3faac4d08d76bd4586b5c98ef84
Piotr Kosmowski <kospiotr@gmail.com>
2017-03-26 13:25:33 +0200

    update

--- tdb-gui/src/app/app.module.ts
+++ tdb-gui/src/app/app.module.ts
@@ -3,15 +3,23 @@ import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
+import { LayoutComponent } from './shared/layout/layout.component';
+import {AppRoutes} from "./app.routes";
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { UsersComponent } from './modules/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    DashboardComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
